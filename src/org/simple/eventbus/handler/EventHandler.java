051011372b349db32a53e19634b9cddded3d9993
hehonghui <hehonghui@umeng.com>
2015-02-02 13:04:16 +0800

    add javadoc

--- src/org/simple/eventbus/handler/EventHandler.java
+++ src/org/simple/eventbus/handler/EventHandler.java
@@ -26,6 +26,17 @@ package org.simple.eventbus.handler;
 
 import org.simple.eventbus.Subscription;
 
+/**
+ * 事件处理接口
+ * 
+ * @author mrsimple
+ */
 public interface EventHandler {
+    /**
+     * 处理事件
+     * 
+     * @param subscription 订阅对象
+     * @param event 待处理的事件
+     */
     void handleEvent(Subscription subscription, Object event);
 }
